2c6a35b6e993167a23c8b3b5be5dd4a9fd98445e
Jon Pastorek <jppastorek@gmail.com>
2022-12-05 22:42:57 -0600

    Styling

--- webpack.config.js
+++ webpack.config.js
@@ -32,10 +32,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
     ],
   },
   watch: true,
